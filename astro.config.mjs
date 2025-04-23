600183af21c9c0028bd87b864eb9fc17d2b76d74
Sidarth G <sidarthdev157@gmail.com>
2025-04-24 02:19:15 +0530

    Changed lock file to text in bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://sidarth.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
 });
