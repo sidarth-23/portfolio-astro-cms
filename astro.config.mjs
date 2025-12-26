703b35aa335108f55837071d660181ff8fc0decd
Sidarth G <58748190+sidarth-23@users.noreply.github.com>
2025-12-26 21:56:44 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://portfolio.sidshub.in",
+  site: "https://www.sidshub.in",
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
