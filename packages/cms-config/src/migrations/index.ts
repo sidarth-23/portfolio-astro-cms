03c058b0b9e800b1eaa07d2ceb43584d237ff0da
Sidarth G <sidarthdev157@gmail.com>
2026-04-10 11:20:06 +0000

    feat: add new migration for CMS configuration
    - Introduce migration 20260410_130000 to the CMS configuration
    - Update migrations index to include the new migration's up and down methods

--- packages/cms-config/src/migrations/index.ts
+++ packages/cms-config/src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260409_213110 from './20260409_213110';
 import * as migration_20260410_044500 from './20260410_044500';
 import * as migration_20260410_044501 from './20260410_044501';
 import * as migration_20260410_120000 from './20260410_120000';
+import * as migration_20260410_130000 from './20260410_130000';
 
 export const migrations = [
   {
@@ -23,5 +24,10 @@ export const migrations = [
     up: migration_20260410_120000.up,
     down: migration_20260410_120000.down,
     name: '20260410_120000'
+  },
+  {
+    up: migration_20260410_130000.up,
+    down: migration_20260410_130000.down,
+    name: '20260410_130000'
   },
 ];
