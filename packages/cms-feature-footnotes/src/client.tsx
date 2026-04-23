f2bb9ce9f0fce87879391518722fe45981d58300
Sidarth G <sidarthdev157@gmail>
2026-04-24 03:45:09 +0530

    feat: implement footnotes feature with client and server nodes, transformers, and utilities

--- packages/cms-feature-footnotes/src/client.tsx
+++ packages/cms-feature-footnotes/src/client.tsx
@@ -1,1 +1,11 @@
-export { FootnotesFeatureClient } from "./footnotes/client";
+"use client";
+import { createClientFeature } from "@payloadcms/richtext-lexical/client";
+import { FootnoteReferenceNode } from "./nodes/FootnoteReferenceNode.client";
+import { FootnoteDefinitionNode } from "./nodes/FootnoteDefinitionNode.client";
+import { FOOTNOTE_REFERENCE_TRANSFORMER } from "./transformers/footnoteReference";
+import { FOOTNOTE_DEFINITION_TRANSFORMER } from "./transformers/footnoteDefinition";
+
+export const FootnotesFeatureClient = createClientFeature({
+  markdownTransformers: [FOOTNOTE_REFERENCE_TRANSFORMER, FOOTNOTE_DEFINITION_TRANSFORMER],
+  nodes: [FootnoteReferenceNode, FootnoteDefinitionNode],
+});
